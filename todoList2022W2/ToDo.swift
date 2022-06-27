0b4f93943b9ba23461167bcce5c0b4ead1a537f2
Matt Wecht <mattwecht@gmail.com>
2022-06-27 16:48:17 -0500

    Finished Iteration 0

---
+++ todoList2022W2/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  todoList2022W2
+//
+//  Created by Scholar on 6/27/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = "THIS IS BAD DONT WANT THIS"
+    var important = false
+}
